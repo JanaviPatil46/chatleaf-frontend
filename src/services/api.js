7b6c7a407bd63a6e07c4eff444cc53de958699da
vinukumbhar <vinayak.intcon@gmail.com>
2026-03-14 18:18:36 +0530

    backend api port chnge

--- src/services/api.js
+++ src/services/api.js
@@ -27,7 +27,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000/api";
+const API_URL = "https://chatleaf-backend.netlify.app/api";
 
 const api = axios.create({
   baseURL: API_URL,
--- src/services/socket.js
+++ src/services/socket.js
@@ -25,7 +25,10 @@
 // src/services/socket.js
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://127.0.0.1:5000";
+// const SOCKET_URL = "http://127.0.0.1:5000";
+
+const SOCKET_URL = "https://chatleaf-backend.netlify.app";
+
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
